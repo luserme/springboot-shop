5e9e15657cacd87f91b66215fc6c70129adfd15d
luserme <wangluobu.luqing@gmail.com>
2018-04-20 22:52:15 +0800

    修改实体类注解内容,修复无法自动创建表的问题

--- src/main/java/com/lq/shop/entity/OrderItemEntity.java
+++ src/main/java/com/lq/shop/entity/OrderItemEntity.java
@@ -22,14 +22,13 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
  */
 @Entity
 @Table(
-    name = "shop_order_item",
-    indexes = {
-        @Index(name = "order_no_index",columnList = "orderNo"),
-        @Index(name = "order_no_user_id_index",columnList = "userId,orderNo")
-    }
+        name = "shop_order_item",
+        indexes = {
+                @Index(name = "order_no_index",columnList = "orderNo"),
+                @Index(name = "order_no_user_id_index",columnList = "userId,orderNo")
+        }
 )
 @EntityListeners(AuditingEntityListener.class)
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -37,29 +36,29 @@ public class OrderItemEntity {
 
     @Id
     @GeneratedValue
-    @Column(columnDefinition = "COMMENT '订单子表id'")
+    @Column(columnDefinition = "INT COMMENT '订单子表id'")
     private int id;
 
     private Integer userId;
 
     private Long orderNo;
 
-    @Column(columnDefinition = "COMMENT '商品id'")
+    @Column(columnDefinition = "INT COMMENT '商品id'")
     private Integer productId;
 
-    @Column(length = 100,columnDefinition = "COMMENT '商品名称'")
+    @Column(length = 100,columnDefinition = "VARCHAR(100) COMMENT '商品名称'")
     private String productName;
 
-    @Column(length = 500,columnDefinition = "COMMENT '商品图片地址'")
+    @Column(length = 500,columnDefinition = "VARCHAR(500) COMMENT '商品图片地址'")
     private String productImage;
 
-    @Column(precision = 20,scale = 2,columnDefinition = "COMMENT '生成订单时的商品单价，单位是元,保留两位小数'")
+    @Column(precision = 20,scale = 2,columnDefinition = "DECIMAL(20, 2) COMMENT '生成订单时的商品单价，单位是元,保留两位小数'")
     private BigDecimal currentUnitPrice;
 
-    @Column(length = 10,columnDefinition = "COMMENT '商品数量'")
+    @Column(length = 10,columnDefinition = "INT COMMENT '商品数量'")
     private Integer quantity;
 
-    @Column(precision = 20,scale = 2,columnDefinition = "COMMENT '商品总价,单位是元,保留两位小数'")
+    @Column(precision = 20,scale = 2,columnDefinition = "DECIMAL(20, 2) COMMENT '商品总价,单位是元,保留两位小数'")
     private BigDecimal totalPrice;
 
     @CreatedDate
