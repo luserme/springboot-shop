26c5e16bd0b4e904c426bf9a7bb9a9f9c5b8f651
luserme <wangluobu.luqing@gmail.com>
2018-05-06 23:47:09 +0800

    新增String工具类isNotEmpty方法

--- src/main/java/com/lq/shop/common/util/StringUtils.java
+++ src/main/java/com/lq/shop/common/util/StringUtils.java
@@ -20,6 +20,11 @@ public class StringUtils {
         return org.apache.commons.lang3.StringUtils.isEmpty(value);
     }
 
+    public static boolean isNotEmpty(String value){
+        return org.apache.commons.lang3.StringUtils.isNotEmpty(value);
+    }
+
+
     public static boolean equals(String forgetToken, String token) {
         return org.apache.commons.lang3.StringUtils.equals(forgetToken,token);
     }
